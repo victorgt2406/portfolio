df170dc8eec9966691e1d4ea719697f2da64d382
Victor <victorgt2406@github.com>
2023-10-13 18:16:28 +0200

    Skills prep

--- client/src/Routes/Home.tsx
+++ client/src/Routes/Home.tsx
@@ -1,6 +1,7 @@
 import Presentation from "../utils/Presentation";
 import Carousel from "../utils/Carousel";
 import NavBar from "../utils/NavBar";
+import Skills from "../utils/Skills";
 
 export default function () {
     return (
@@ -9,6 +10,7 @@ export default function () {
             <div className="w-100 my-2 d-flex flex-column align-items-center  container">
                 <Presentation />
                 <Carousel />
+                <Skills/>
             </div>
         </>
     );
---
+++ client/src/utils/Skills.tsx
@@ -0,0 +1,22 @@
+
+type SkillType = {
+    title: string;
+    icon: JSX.Element;
+    lables: string[];
+}
+
+function Skill(){
+    return (
+    <div>
+
+    </div>
+    );
+}
+
+export default function(){
+    return (
+        <div className="d-flex">
+
+        </div>
+    );
+}

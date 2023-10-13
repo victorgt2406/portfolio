b95710698dff06e02597bf81e98d47106308b653
Victor <victorgt2406@github.com>
2023-10-13 17:16:31 +0200

    CarrouselItem divided

--- client/src/utils/Carousel.tsx
+++ client/src/utils/Carousel.tsx
@@ -1,35 +1,6 @@
 import CarouselProject from "./CarouselItems/CarouselProject";
 import IconLink, { IconLinkType } from "./IconLink";
 
-function CarrouselItem({
-    title,
-    description,
-    links,
-}: {
-    title: string;
-    description: string;
-    links: IconLinkType[];
-}) {
-    return (
-        <div className="carousel-item">
-            <div className="d-flex justify-content-center">
-                <img
-                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu_%28cropped%29.jpg/1200px-View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu_%28cropped%29.jpg"
-                    className="col-xs-8 col"
-                    height={"375px"}
-                />
-                <div className="col-xs-4 col d-flex flex-column ps-3 p-2">
-                    <h3>{title}</h3>
-                    <p>{description}</p>
-                    <div className="mt-auto">
-                        {...links.map((link) => <IconLink {...link} />)}
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
-}
-
 export default function () {
     return (
             <div 

b7ba92283f4bec6df086dbdb6c6519b4110f8148
vic <victorgt2406@github.com>
2024-03-19 00:03:18 +0100

    Position improved

--- client/src/Routes/Home/Experiences/Position.tsx
+++ client/src/Routes/Home/Experiences/Position.tsx
@@ -1,19 +1,34 @@
-
-
 type MyProps = {
     title: string;
     description: string[];
     skills: string[];
-}
-
+};
 
 export default function Position({ title, description, skills }: MyProps) {
+    const descriptionElements = description.map((desc, index) => (
+        <li key={index} className="my-1 fw-light">{desc}</li>
+    ));
+
     return (
         <div className="d-flex flex-column">
-            <div className="ps-2">{title}</div>
-            <div className="ps-2">{...description}</div>
-            <div className="ps-2 d-flex justify-content-start align-items-center w-100 flex-wrap">
-                {...skills.map((skill, index) => <><span className="text-center fw-light" key={index}>{skill}</span>{index===skills.length-1?"":<div className="me-2">, </div>}</>)}
+            <div className="fs-5 fw-bold ps-2">{title}</div>
+            <div className="ps-1">
+                <ul>{...descriptionElements}</ul>
+            </div>
+            <div className="ps-2 pt-3 d-flex justify-content-start align-items-center w-100 flex-wrap">
+                <span className="me-2">Skills:</span>
+                {...skills.map((skill, index) => (
+                    <>
+                        <span className="text-center fw-light fst-italic" key={index}>
+                            {skill}
+                        </span>
+                        {index === skills.length - 1 ? (
+                            ""
+                        ) : (
+                            <div className="me-2">, </div>
+                        )}
+                    </>
+                ))}
             </div>
         </div>
     );

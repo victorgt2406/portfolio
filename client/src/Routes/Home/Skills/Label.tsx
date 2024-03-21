d4cda13bb543b008164b9c62f01d4fe86104486e
vic <victorgt2406@github.com>
2024-03-21 14:02:59 +0100

    Label animation

--- client/src/Routes/Home/Skills/Label.tsx
+++ client/src/Routes/Home/Skills/Label.tsx
@@ -1,14 +1,37 @@
+import { motion } from "framer-motion";
 import { ReactNode } from "react";
 
 type MyProps = {
     isSelected: boolean;
     children: ReactNode;
     title: string;
-    setLabel: (label:string)=>void
-}
+    setLabel: (label: string) => void;
+};
 
-export default function Label ({children, title, setLabel, isSelected}: MyProps) {
+export default function Label({
+    children,
+    title,
+    setLabel,
+    isSelected,
+}: MyProps) {
+    const backgroundColor = isSelected
+        ? "var(--bs-primary)"
+        : "var(--bs-secondary)";
+
+    const hoverColor = "var(--bs-primary)";
+
+
+    const scale = 0.95;
+
     return (
-        <div className={`m-2 btn badge rounded-pill ${isSelected?"text-bg-primary":"text-bg-secondary"} fs-5`} onClick={()=>setLabel(title)}>{children}</div>
+        <motion.div
+            whileHover={{ backgroundColor: hoverColor, scale }}
+            style={{ backgroundColor: backgroundColor }}
+            transition={{ duration: 0.2, ease: "easeInOut" }}
+            className={`m-2 btn badge rounded-pill text-white fs-5`}
+            onClick={() => setLabel(title)}
+        >
+            {children}
+        </motion.div>
     );
 }

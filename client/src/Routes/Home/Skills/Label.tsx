f25fbc2d08a9f3fd6bae0f6b03a9745e3b2f13d4
vic <victorgt2406@github.com>
2024-03-21 23:36:50 +0100

    labels snappier

--- client/src/Routes/Home/Skills/Label.tsx
+++ client/src/Routes/Home/Skills/Label.tsx
@@ -14,21 +14,14 @@ export default function Label({
     setLabel,
     isSelected,
 }: MyProps) {
-    const backgroundColor = isSelected
-        ? "var(--bs-primary)"
-        : "var(--bs-secondary)";
 
-    const hoverColor = "var(--bs-primary)";
-
-
-    const scale = 0.95;
+    const scale = 0.98;
 
     return (
         <motion.div
-            whileHover={{ backgroundColor: hoverColor, scale }}
-            style={{ backgroundColor: backgroundColor }}
-            transition={{ duration: 0.2, ease: "easeInOut" }}
-            className={`m-2 btn badge rounded-pill text-white fs-5`}
+            whileHover={{ scale }}
+            transition={{ duration: 0.075 }}
+            className={`m-2 btn badge rounded-pill ${isSelected?"text-bg-primary":"text-bg-dark"} fs-5`}
             onClick={() => setLabel(title)}
         >
             {children}

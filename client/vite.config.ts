d92fcf3db9634716e58e1a3ea641b746b12cf77a
vic <victorgt2406@github.com>
2024-03-22 12:28:12 +0100

    vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: "0.0.0.0" // Listens on all local IPs. Replace `true` with your actual IP if needed.
+  }
 })

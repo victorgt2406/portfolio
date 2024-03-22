55269eb30479653048e21e5fd46af635a377e70b
vic <victorgt2406@github.com>
2024-03-22 11:03:41 +0100

    Navbar

--- client/src/Components/NavBar/index.tsx
+++ client/src/Components/NavBar/index.tsx
@@ -1,46 +1,22 @@
-// import { useContext } from "react";
-// import Context from "../../utils/Context";
-// import Select from "../Select";
+import { useContext } from "react";
+import Context from "../../utils/Context";
 import Element from "./Element";
-// import Spacer from "./Spacer";
-// import { DARKMODES, Darkmode, LANGS, Lang } from "../../utils/types";
+import capitalizeFirstLetter from "../../utils/capitalizeFirstLetter";
 
 export default function NavBar() {
-    // const { dark, setDark, lang, setLang } = useContext(Context);
+    const { lang } = useContext(Context);
 
     const elements: JSX.Element[] = [
         <Element
-            title={"Home"}
+            title={capitalizeFirstLetter(lang.home)}
             link={"/"}
             icon={<i className="bi bi-house-fill"></i>}
         />,
         <Element
-            title={"Contact"}
-            link={"contact"}
+            title={capitalizeFirstLetter(lang.contact)}
+            link={"/contact"}
             icon={<i className="bi bi-person-lines-fill"></i>}
-        />,
-        // <Spacer/>,
-        // <Element
-        //     title={"Language: "}
-        //     icon={<i className="bi bi-translate"></i>}
-        // />
-        // ,
-        // <Select
-        //     options={LANGS}
-        //     title={lang}
-        //     onChange={(option: string) => {
-        //         setLang(option as Lang);
-        //     }}
-        // />,
-        // <Spacer/>,
-        // <Element title={"Darkmode: "} icon={<i className="bi bi-moon"></i>} />,
-        // <Select
-        //     options={DARKMODES}
-        //     title={dark}
-        //     onChange={(option: string) => {
-        //         setDark(option as Darkmode);
-        //     }}
-        // />,
+        />
     ];
     return (
         <div

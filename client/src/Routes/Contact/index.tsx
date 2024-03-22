1126d18c1e4ed1e7b0f9376f9989371480146a28
vic <victorgt2406@github.com>
2024-03-22 20:06:15 +0100

    contact form

--- client/src/Routes/Contact/index.tsx
+++ client/src/Routes/Contact/index.tsx
@@ -20,6 +20,45 @@ export default function Contact() {
                     icon={<i className="bi bi-github"></i>}
                     info={"https://github.com/victorgt2406"}
                 />
+                <hr className="w-100" />
+                <div
+                    className="w-100 my-3"
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                    <style>
+                    .responsive-iframe {
+                        width: 100%;
+                        border: none;
+                        height: 800px; 
+                    }
+                    @media (max-width: 768px) {
+                        .responsive-iframe {
+                            /* Height for mobile devices */
+                            height: 1100px;
+                        }
+                    }
+                    </style>
+
+                    <iframe class="responsive-iframe" src="https://docs.google.com/forms/d/e/1FAIpQLSdAa6IL_1Lt5p-VDgjYixQKfgHJ1VtZP0imn8uhi49SNIjfoQ/viewform?embedded=true" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+
+                    <script>
+                    function adjustIframeHeight() {
+                    var iframe = document.querySelector('.responsive-iframe');
+                    // Check if the device's viewport width is less than or equal to 768px, considered mobile
+                    if (window.innerWidth <= 768) {
+                        // Height for mobile devices
+                        iframe.style.height = "1100px";
+                    } else {
+                        // Height for desktop
+                        iframe.style.height = "800px";
+                    }
+                    }
+                    window.addEventListener('load', adjustIframeHeight);
+                    window.addEventListener('resize', adjustIframeHeight);
+                    </script>
+                  `,
+                    }}
+                />
             </div>
         </div>
     );

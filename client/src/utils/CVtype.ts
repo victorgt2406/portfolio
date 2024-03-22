603ec582940a5be670178cf2620da5804f31dc6c
vic <victorgt2406@github.com>
2024-03-22 10:25:24 +0100

    Fixing Cv type

--- client/src/utils/CVtype.ts
+++ client/src/utils/CVtype.ts
@@ -1,49 +1,59 @@
-type CV = {
-    presentation: {
-        fullname: string;
-        name: string;
-        surnames: string;
-    };
-    skills: Array<{
-        title: string;
-        icon: string;
-        labels: string[];
-    }>;
-    education: Array<{
-        company: string;
-        icon: string;
-        location: string;
+type Presentation = {
+    fullname: string;
+    name: string;
+    surnames: string;
+};
+
+type Skills = {
+    title: string;
+    icon: string;
+    labels: string[];
+};
+
+type Education = {
+    company: string;
+    icon: string;
+    location: string;
+    start: string;
+    end: string;
+    years: Array<{
         start: string;
         end: string;
-        years: Array<{
-            start: string;
-            end: string;
-            title: string;
-            honors: string[];
-        }>;
-    }>;
-    experience: Array<{
-        company: string;
-        image: string;
-        position: {
-            title: string;
-            description: string[];
-            skills: string[];
-        };
-        start: string;
-        end: string;
-        location: string;
-    }>;
-    projects: Array<{
-        id: string;
         title: string;
-        description: string;
-        img: string;
-        links: Array<{
-            title: string;
-            url: string;
-        }>;
+        honors: string[];
     }>;
 };
 
-export type { CV };
+type Experience = {
+    company: string;
+    image: string;
+    position: {
+        title: string;
+        description: string[];
+        skills: string[];
+    };
+    start: string;
+    end: string;
+    location: string;
+};
+
+type Projects = {
+    id: string;
+    title: string;
+    description: string;
+    img: string;
+    links: Array<{
+        title: string;
+        url: string;
+    }>;
+};
+
+type CV = {
+    presentation: Presentation;
+    skills: Array<Skills>;
+    education: Array<Education>;
+    experience: Array<Experience>;
+    projects: Array<Projects>;
+};
+
+export type { CV, Presentation, Skills, Education, Experience, Projects };

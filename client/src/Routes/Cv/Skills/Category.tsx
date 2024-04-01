502511274103f50da9d9b002e93841ae4133fbcf
vic <victorgt2406@github.com>
2024-04-01 15:51:21 +0200

    fix skills categories size

--- client/src/Routes/Cv/Skills/Category.tsx
+++ client/src/Routes/Cv/Skills/Category.tsx
@@ -4,9 +4,9 @@ import { SkillsCategories } from "../../../types/CVtypes";
 
 export default function Category({category, skills}:SkillsCategories){
     return (
-        <div className="d-flex col-sm-6 col-12 fs-8">
-            <span className="fw-bold me-2">{category}:</span>
-            <span className="fw-light fst-italic">{skills.join(", ")}</span>
+        <div className="d-flex align-items-center col-sm-6 col-12 px-1">
+            <span className="fw-bold me-2 fs-7">{category}:</span>
+            <span className="fw-light fst-italic fs-8">{skills.join(", ")}</span>
         </div>
     );
 }
